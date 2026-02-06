3d5bb9859c910728dfe5e5edd4681b18a9e38418
Codeling <kostyazhilov03@gmail.com>
2026-02-06 15:53:48 +0300

    add debugging

--- sources/albumoftheflac/orchestrator.py
+++ sources/albumoftheflac/orchestrator.py
@@ -18,15 +18,19 @@ from albumoftheflac.text import replace_spaces_with_pluses
 async def set_correct_tags(directory: Path):
     # get artist and album tags
     tags = export_tags(directory)
+    print(f"tags: {tags}")
     artist = get_tag(tags, "artist")
     album = get_tag(tags, "album")
+    print(f"artist and album tags: {artist}, {album}")
 
     # make artist and album tags suitable for links
     artist_tag = replace_spaces_with_pluses(artist)
     album_tag = replace_spaces_with_pluses(album)
+    print(f"artist tag, album tag: {artist_tag}, {album_tag}")
 
     # get album link from search query
     link_for_search = create_link_for_search(artist_tag, album_tag)
+    print(f"link for search: {link_for_search}")
     search_content = fetch_page(link_for_search)
     album_link = get_album_link(search_content)
     print(f"album link: {album_link}")
