1044d0743ac0c6e82837424e88f0e2a1f4c88cfc
Codeling <kostyazhilov03@gmail.com>
2026-02-09 16:22:27 +0300

    remove debug messages

--- sources/albumoftheflac/tags.py
+++ sources/albumoftheflac/tags.py
@@ -27,7 +27,6 @@ def get_tag(tags: str, tag: str) -> str:
         if "=" not in line:
             continue
         k, v = line.split("=", 1)
-        print(f"DEBUGGING: tags.get_tag(): k: {k}, v: {v}, tag: {tag}")
         if tag == k:
             return v
     return ""
